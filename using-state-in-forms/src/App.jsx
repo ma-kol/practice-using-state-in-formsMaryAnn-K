192595e9eb6d701770c2c2f43bbe1548f9f03c34
Mary Ann Koller <171463891+ma-kol@users.noreply.github.com>
2025-10-23 20:29:24 -0400

    Practice Using State in Forms/Group Practice

---
+++ using-state-in-forms/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import UserForm from './components/FormData'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+
+      <UserForm />
+
+    </div>
+
+  )
+}
+export default App;
